9f661a04fa204b0f3fb50a3b874abd602e8c88d3
Christian Hizon <christiangerardhizon@gmail.com>
2018-08-19 03:02:34 +0800

    Search Changes
    added changes to the main.css

--- modules/client/search/search.php
+++ modules/client/search/search.php
@@ -1,1 +1,27 @@
-SEARCh
+<!-- Main -->
+<section id="main" class="wrapper">
+        <div class="inner">
+            <div class="content">
+
+            <!-- Elements -->
+                <h3> Food </h3>
+                <div class="row">
+                    <div class="col-12">
+                        <input type="text" name="enter a food name" id="food" value="" placeholder="Name" />
+                    </div>
+                </div>
+                <br/>
+                <h3> Budget </h3>
+                <div class="row">
+                    <div class="col-6">
+                        <h5> Minimum </h5>
+                        <input type="number" name="enter a food name" id="food" value="" placeholder="Name" />
+                    </div>
+                    <div class="col-6">
+                        <h5> Maximum </h5>
+                        <input type="number" name="enter a food name" id="food" value="" placeholder="Name" />
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
