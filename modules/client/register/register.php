c465ed485e49d67778b1c6de9e83669ed342bc21
Rigel Lago <rigellago2013@gmail.com>
2018-08-29 00:31:05 +0800

    lakad matatag normalin normalin

--- modules/client/register/register.php
+++ modules/client/register/register.php
@@ -4,13 +4,26 @@
 
 	$users = new User($connection);
 
+	$output = array();
+
 	if($_POST['password'] == $_POST['confirmpassword']) {
 
 		$register = $users->register($_POST['name'], $_POST['email'], md5($_POST['password']));
 
+
+		if($register) {
+
+				$output = array('response' => "Account created succssfuly.");
+	
+		} else {
+
+			$output = array('response' => "Whoops! Looks like there's something wrong..");
+		}
+
 	} else {
 
-		echo "Please match passwords.";
+		 $output = array('response' => "Please match password");
 	}
 
-?>
+	echo json_encode($output);
+?>	
